df51235f9234612fc1e1efa69a62311eff09ba37
Leo Alt <leo@ethereum.org>
2022-01-10 21:52:14 +0100

    Replace declare-const and member function applications

--- d.smt2
+++ d.smt2
@@ -1,13 +1,16 @@
-(declare-datatypes ((|state_type| 0)) (((|state_type| (|balances| (Array Int Int))))))
+(declare-datatypes ((|state_type| 0)) (((|state_type| (|balances| (Array Int Int)) (|a| Int)  ))))
+
+(declare-fun f ( (Array Int Int) Int) Bool)
 
 (declare-const x Int)
 (declare-const y Int)
 (declare-const s |state_type|)
+(declare-const s2 |state_type|)
 
 (assert
 (and
 	(= x (select (|balances| s) 1))
-	(= y (select (|balances| s) 2))
+	(= y (select (|balances| s2) 2))
 	(= x y)
 ))
 
--- src/transform.rs
+++ src/transform.rs
@@ -1,8 +1,12 @@
-use amzn_smt_ir::{logic::*, Command as IRCommand, ParseError, Script, Term as IRTerm};
-use amzn_smt_ir::{ICoreOp, ISort, ISymbol, IVar, QualIdentifier, Identifier};
-use amzn_smt_ir::visit::{ControlFlow, Visit, Visitor, SuperVisit};
-use amzn_smt_ir::Term::Quantifier;
+use amzn_smt_ir::fold::{Fold, Folder, SuperFold};
+use amzn_smt_ir::Quantifier::Exists;
 use amzn_smt_ir::Quantifier::Forall;
+use amzn_smt_ir::Term::Quantifier;
+use amzn_smt_ir::{logic::*, Command as IRCommand, ParseError, Script, Term as IRTerm};
+use amzn_smt_ir::{IConst, ICoreOp, ILet, IMatch, IQuantifier, IUF};
+use amzn_smt_ir::{IIdentifier, IOp, ISort, ISymbol, IVar, Identifier, QualIdentifier};
+
+use smt2parser::concrete::Command::DeclareConst;
 
 use num_traits::identities::Zero;
 use std::collections::HashMap;
@@ -14,49 +18,134 @@ type Command = IRCommand<Term>;
 /// Build an iterator from an input buffer. The input is parsed as a list of commands
 /// in SMT-LIB syntax.
 ///
-pub fn parse(
-    smtlib: impl std::io::BufRead,
-) -> Result<Script<Term>, ParseError<ALL>> {
-    Ok(Script::<Term>::parse(smtlib)?)
+pub fn parse(smtlib: impl std::io::BufRead) -> Result<Script<Term>, ParseError<ALL>> {
+    Script::<Term>::parse(smtlib)
 }
 
 #[derive(Default, Clone, Debug)]
 pub struct ADTFlattener {
-    // datatype_sort -> member_sort -> sort
-    datatypes: HashMap<ISymbol, HashMap<ISymbol, ISort>>,
+    // datatype_sort -> member_sort -> (template name, sort)
+    datatypes: HashMap<ISymbol, HashMap<ISymbol, (ISymbol, ISort)>>,
     // variable name -> sort
     var_sorts: HashMap<ISymbol, ISort>,
-    local_var_sorts: HashMap<ISymbol, ISort>,
 }
 
-impl Visitor<ALL> for ADTFlattener {
-    type BreakTy = ();
+fn identifier_symbol(identifier: &IIdentifier) -> ISymbol {
+    match identifier.as_ref() {
+        Identifier::Simple { symbol } => symbol.clone(),
+        Identifier::Indexed { symbol, .. } => symbol.clone(),
+    }
+}
 
-    fn visit_var(&mut self, var: &IVar<QualIdentifier>) -> ControlFlow<Self::BreakTy> {
-        if let QualIdentifier::Simple { identifier } = var.as_ref() {
-            let id_symbol = match identifier.as_ref() {
-                Identifier::Simple { symbol } => symbol,
-                Identifier::Indexed { symbol, .. } => symbol
-            };
-            match self.var_sorts.get(id_symbol) {
-                Some(sort) => {
-                    println!("Variable {} has sort {}", identifier, sort.sym())
-                }
-                None => {
-                    println!("Variable {} has no sort", identifier)
+fn var_symbol(var: &IVar<QualIdentifier>) -> ISymbol {
+    match var.as_ref() {
+        QualIdentifier::Simple { identifier } => identifier_symbol(identifier),
+        QualIdentifier::Sorted { identifier, .. } => identifier_symbol(identifier),
+    }
+}
+
+impl Folder<ALL> for ADTFlattener {
+    type Output = Term;
+    type Error = ();
+
+    fn fold_uninterpreted_func(&mut self, uf: IUF<ALL>) -> Result<Self::Output, Self::Error> {
+        let args = &uf.as_ref().args;
+        if args.len() == 1 {
+            if let Term::Variable(var) = &args[0] {
+                let sort = self.var_sort(var);
+                if let Some(dt) = self.datatypes.get(sort.sym()) {
+                    let dt_info = dt.get(&uf.as_ref().func);
+                    assert!(dt_info.is_some());
+                    let new_name =
+                        ISymbol::from(format!("{}_{}", dt_info.unwrap().0, var_symbol(var)));
+                    return Ok(Term::from(IVar::from(QualIdentifier::from(new_name))));
                 }
             }
         }
-        ControlFlow::CONTINUE
+
+        uf.super_fold_with(self).map(Into::into)
     }
 
-    fn visit_core_op(&mut self, op: &ICoreOp<ALL>) -> ControlFlow<Self::BreakTy> {
-        op.super_visit_with(self)
+    fn fold_const(&mut self, constant: IConst) -> Result<Self::Output, Self::Error> {
+        Ok(constant.into())
+    }
+
+    fn fold_var(&mut self, var: IVar<QualIdentifier>) -> Result<Self::Output, Self::Error> {
+        Ok(var.into())
+    }
+
+    fn fold_core_op(&mut self, op: ICoreOp<ALL>) -> Result<Self::Output, Self::Error> {
+        op.super_fold_with(self).map(Into::into)
+    }
+
+    fn fold_theory_op(&mut self, op: IOp<ALL>) -> Result<Self::Output, Self::Error> {
+        op.super_fold_with(self).map(Into::into)
+    }
+
+    fn fold_let(&mut self, l: ILet<ALL>) -> Result<Self::Output, Self::Error> {
+        l.super_fold_with(self).map(Into::into)
+    }
+
+    fn fold_match(&mut self, m: IMatch<ALL>) -> Result<Self::Output, Self::Error> {
+        m.super_fold_with(self).map(Into::into)
+    }
+
+    fn fold_quantifier(
+        &mut self,
+        quantifier: IQuantifier<ALL>,
+    ) -> Result<Self::Output, Self::Error> {
+        quantifier.super_fold_with(self).map(Into::into)
     }
 }
 
 impl ADTFlattener {
-    pub fn collect_datatypes(&mut self, commands: &Script<Term>) {
+    pub fn flatten(&mut self, commands: Script<Term>) -> Script<Term> {
+        self.collect_datatypes(&commands);
+
+        self.collect_var_sorts(&commands);
+
+        let commands = self.remove_declare_datatypes(commands);
+        let commands = self.flatten_all_declare_const(commands);
+
+        // Flatten each assertion separately.
+        commands.map(|cmd| match cmd {
+            IRCommand::Assert { term } => self.flatten_assertion(term),
+            _ => cmd,
+        })
+    }
+
+    fn var_sort(&self, var: &IVar<QualIdentifier>) -> ISort {
+        self.var_sorts.get(&var_symbol(var)).unwrap().clone()
+    }
+
+    fn flatten_assertion(&mut self, assertion: Term) -> IRCommand<Term> {
+        //let quant_vars = self.collect_quant_var_sorts(&assertion);
+
+        /*
+        if let Quantifier(ref quantifier) = assertion {
+            if let Forall(vars, term) = quantifier.as_ref() {
+                let assertion = Forall(vars.to_vec(), term);
+            } else if let Exists(vars, term) = quantifier.as_ref() {
+                let assertion = Exists(vars.to_vec(), term);
+            }
+        }
+        */
+
+        /*
+        let assertion = match assertion {
+            Quantifier(quantifier) => Quantifier(IQuantifier(match quantifier.as_ref() {
+                Forall(vars, term) => Forall(self.flatten_var_decls(vars.to_vec()), term),
+                Exists(vars, term) => Exists(self.flatten_var_decls(vars.to_vec()), term)
+            })),
+            _ => assertion
+        };
+        */
+
+        let t = assertion.fold_with(self);
+        IRCommand::Assert { term: t.unwrap() }
+    }
+
+    fn collect_datatypes(&mut self, commands: &Script<Term>) {
         commands.as_ref().iter().for_each(|c| {
             if let Command::DeclareDatatypes { datatypes } = c {
                 datatypes
@@ -69,46 +158,69 @@ impl ADTFlattener {
                             .selectors
                             .clone()
                             .into_iter()
-                            .collect::<HashMap<ISymbol, ISort>>();
+                            .map(|(member, sort)| {
+                                (
+                                    member.clone(),
+                                    (ISymbol::from(format!("{}_{}", symbol, member)), sort),
+                                )
+                            })
+                            .collect::<HashMap<ISymbol, (ISymbol, ISort)>>();
                         self.datatypes.insert(symbol.clone(), members);
                     })
             }
         });
-
-        println!("{:?}", self.datatypes);
     }
 
-    pub fn collect_var_sorts(&mut self, commands: &Script<Term>) {
+    fn collect_var_sorts(&mut self, commands: &Script<Term>) {
         commands.as_ref().iter().for_each(|c| {
             if let Command::DeclareConst { symbol, sort } = c {
                 self.var_sorts.insert(symbol.clone(), sort.clone());
             }
         });
-
-        println!("{:?}", self.var_sorts);
     }
 
-    pub fn collect_quant_var_sorts(&mut self, commands: &Script<Term>) {
-        commands.as_ref().iter().for_each(|c| {
-            if let Command::Assert { term } = c {
-                println!("{:?}", term);
-                if let Quantifier(quantifier) = term.clone() {
-                    if let Forall(vars, ..) = quantifier.as_ref() {
-                        vars.into_iter().for_each(|(symbol, sort)| {
-                            self.local_var_sorts.insert(symbol.clone(), sort.clone());
-                        })
-                    }
-                }
-                let flow = term.visit_with(self);
-                assert!(flow == ControlFlow::CONTINUE);
-            }
-        });
+    fn flatten_var_decl(&mut self, (name, sort): (ISymbol, ISort)) -> Vec<(ISymbol, ISort)> {
+        match self.datatypes.get(sort.sym()) {
+            Some(members) => members
+                .iter()
+                .map(|(_, (template_name, sort))| {
+                    (
+                        ISymbol::from(format!("{}_{}", name, template_name)),
+                        sort.clone(),
+                    )
+                })
+                .collect::<Vec<(ISymbol, ISort)>>(),
+            None => vec![(name, sort)],
+        }
     }
 
-    pub fn flatten(&mut self, commands: Script<Term>) -> Script<Term> {
-        self.collect_datatypes(&commands);
-        self.collect_var_sorts(&commands);
-        self.collect_quant_var_sorts(&commands);
+    fn flatten_declare_const(&mut self, declare_const: IRCommand<Term>) -> Vec<IRCommand<Term>> {
+        match declare_const {
+            DeclareConst { symbol, sort } => self
+                .flatten_var_decl((symbol, sort))
+                .into_iter()
+                .map(|(symbol, sort)| IRCommand::DeclareConst { symbol, sort })
+                .collect(),
+            _ => vec![declare_const],
+        }
+    }
+
+    fn flatten_all_declare_const(&mut self, commands: Script<Term>) -> Script<Term> {
+        Script::from_commands(
+            commands
+                .into_iter()
+                .map(|cmd| self.flatten_declare_const(cmd))
+                .collect::<Vec<Vec<IRCommand<Term>>>>()
+                .into_iter()
+                .flatten()
+                .collect::<Vec<IRCommand<Term>>>(),
+        )
+    }
+
+    fn remove_declare_datatypes(&mut self, commands: Script<Term>) -> Script<Term> {
         commands
+            .into_iter()
+            .filter(|cmd| !matches!(cmd, IRCommand::DeclareDatatypes { .. }))
+            .collect()
     }
 }
